9e50c2e6d1385592d388a51a57aae273ac5225a3
Sylvain Yabre <sylvainyabre7@gmail.com>
2023-01-22 11:28:14 -0800

    m

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "http://localhost:3000"
-export const API = axios.create({baseURL:"http://localhost:8000"});
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "http://localhost:3000/"
+export const API = axios.create({baseURL:"https://backend-production-4240.up.railway.app/"});
